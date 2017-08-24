a1362e37bf3a947d300b91e751334e7736f59f4e
vannucci@gmail.com <michaelvannucci@Michaels-MacBook-Air.local>
2017-08-24 16:52:55 -0400

    Basic web interface, with planets and dropdown to create new passenger

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require('express-handlebars');
+var path = require('path');
+var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(express.static('public'));
+
+
+
+
+
+var port = 3000;
+
+
+app.get('/', function(req,res,next) {
+	res.send("hello");
+});
+
+app.listen(port, function() {
+	console.log('App running on port ' + port);
+});
