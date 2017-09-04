a057d999ba17e050116178ec0699d5ecf8efbdb4
vannucci@gmail.com <vannucci@gmail.com>
2017-09-04 09:16:21 -0400

    Added a check for velocity so that if two shuttles are equidistant the shuttle which is moving towards the passenger gets the ticket. In the case that they are both velocity zero and equidistant, the first shuttle will get the ticket

--- model/Dispatcher.js
+++ model/Dispatcher.js
@@ -18,15 +18,28 @@ function Dispatcher(solarsystem,shuttle1, shuttle2) {
 	//
 	this.sendShuttle = function (passenger) {
 
-		var shuttleScore = [0,0];
+		//ICE, return shuttle 1. ShuttlePScore is positional score, ShuttleVScore is velocity score
+		var chosenShuttle = 1;
+		var shuttlePScore = [0,0];
+		var shuttleVScore = [0,0];
 
-		shuttleScore[0] = Math.abs(passenger.origin - shuttles[0].currentLocation);
-		console.log(shuttleScore[0]);
+		shuttlePScore[0] = Math.abs(passenger.origin - shuttles[0].currentLocation);
+		shuttleVScore[0] = shuttles[0].velocity * (passenger.origin - shuttles[0].currentLocation);
+		console.log("Shuttle 1 position score " + shuttlePScore[0] + ", shuttle velocity score " + shuttleVScore[0]);
 
-		shuttleScore[1] = Math.abs(passenger.origin - shuttles[1].currentLocation);
-		console.log(shuttleScore[1]);
+		shuttlePScore[1] = Math.abs(passenger.origin - shuttles[1].currentLocation);
+		shuttleVScore[1] = shuttles[1].velocity * (passenger.origin - shuttles[1].currentLocation);
+		console.log("Shuttle 1 position score " + shuttlePScore[1] + ", shuttle velocity score " + shuttleVScore[1]);
 
-		var chosenShuttle = returnLowestScoreIndex(shuttleScore) + 1;
+
+		//If the two shuttles are equidistant from the request origin, go by velocity. This will also work if both shuttles
+		//have velocity zero, since they are both fair candidates and the choice doesn't matter.
+		if(shuttlePScore[0] === shuttlePScore[1]) {
+			chosenShuttle = returnLowestScoreIndex(shuttleVScore) + 1;
+		} else {
+			chosenShuttle = returnLowestScoreIndex(shuttlePScore) + 1;
+		}
+
 		currentSolarSystem.shuttles[chosenShuttle - 1].queuePickupLocation(passenger.origin);
 		return chosenShuttle;
 
@@ -44,7 +57,7 @@ function Dispatcher(solarsystem,shuttle1, shuttle2) {
 		}
 
 		return minIndex;
-	}  
+	};
 
 	console.log("Dispatcher running");
 	
